6fe8255802c0cf586f992176086fc9e3f126c7c7
ianvanharten <ianvharten@gmail.com>
2021-01-15 17:34:39 -0500

    Begin setting up HomeController

--- HistoryBookClub/HistoryBookClub/Controllers/HomeController.cs
+++ HistoryBookClub/HistoryBookClub/Controllers/HomeController.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HistoryBookClub.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HistoryBookClub.Controllers
 {
     public class HomeController : Controller
     {
+        private HistoryBookClubContext context { get; set; }
+
+        public HomeController(HistoryBookClubContext ctx)
+        {
+            context = ctx;
+        }
+
         public IActionResult Index()
         {
             return View();
