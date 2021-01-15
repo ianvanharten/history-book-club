e0d935912d80c05bf3b0633189e14bc008218152
ianvanharten <ianvharten@gmail.com>
2021-01-14 19:21:26 -0500

    Add properties to Book model

--- HistoryBookClub/HistoryBookClub/Models/Book.cs
+++ HistoryBookClub/HistoryBookClub/Models/Book.cs
@@ -7,5 +7,8 @@ namespace HistoryBookClub.Models
 {
     public class Book
     {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
     }
 }
